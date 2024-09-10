ee2814fa3a1fa0e1d5dc59946a4dc9dccb09bf12
Madino <89445570+Maldin0@users.noreply.github.com>
2024-09-10 18:09:30 +0700

    Init Model

--- app/models.py
+++ app/models.py
@@ -2,5 +2,39 @@ from django.db import models
 
 # Create your models here.
 
-class Table:
-    pass
+class Table(models.Model):
+    class Status(models.TextChoices):
+        AVAILABLE = 'AVAILABLE'
+        OCCUPIED = 'OCCUPIED'
+
+    number = models.IntegerField()
+    status = models.CharField(choices=Status.choices, default=Status.AVAILABLE)
+
+class DishCategory(models.Model):
+    name = models.CharField(max_length=150)
+
+class Dish(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField(DishCategory)
+
+class TableCart(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+
+class TableCartItem(models.Model):
+    tableOrder = models.ForeignKey(TableCart, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=1)
+
+class Order(models.Model):
+    table = models.ForeignKey(Table, on_delete=models.CASCADE)
+    order_date = models.DateField()
+    remark = models.TextField(null=True, blank=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    dish = models.ForeignKey(Dish, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=1)
