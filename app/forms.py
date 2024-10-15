ebdd3d8df676e953d8d5f9dbc5ef29be74256e9a
Sweet eyes <65070047@KMITL.AC.TH>
2024-10-16 02:27:28 +0700

    form editing

--- app/forms.py
+++ app/forms.py
@@ -66,20 +66,3 @@ class DishesFilterForm(forms.ModelForm):
             self.add_error('price_max', 'ราคาสูงสุดต้องมากกว่าราคาขั้นต่ำ')
 
         return cleaned_data
-
-class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-    
-        if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email already exists")
-    
-        return email
